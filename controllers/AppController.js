f81b7ab66a1ba30da19de2302a138e3b72222476
abybells <abbiolbello@yahoo.com>
2023-03-07 07:04:05 -0800

    file manager

--- controllers/AppController.js
+++ controllers/AppController.js
@@ -3,9 +3,7 @@ import dbClient from '../utils/db';
 
 class AppController {
   static getStatus(request, response) {
-    response.status(200).json({
-      redis: redisClient.isAlive(),
-      db: dbClient.isAlive() });
+    response.status(200).json({ redis: redisClient.isAlive(), db: dbClient.isAlive() });
   }
 
   static async getStats(request, response) {
